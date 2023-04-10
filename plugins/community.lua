f5550e054a5c148cbe0a7d1212a71f0844cdf09e
Bryan Roberts <roberts@amyris.com>
2023-04-10 13:04:17 -0700

    add community python pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,10 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.colorscheme.tokyonight" },
   { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.pack.python" },
   {
     -- further customize the options set by the community
     "copilot.lua",
@@ -24,5 +23,4 @@ return {
       },
     },
   },
-
 }
